cf6d4f0f4f136d4b1fe0af706f50f9bd80067430
Hai Truong DFI <hai.truong@datafluct.com>
2023-01-09 20:14:35 +0700

    remove Delete button in admin section of Fcm model

--- fcm/admin.py
+++ fcm/admin.py
@@ -6,6 +6,9 @@ from .models import FcmRegistrationId
 # Register your models here.
 class FcmRegistrationIdAdmin(admin.ModelAdmin):
     list_display = ("user", "deviceType", "registrationId")
+    # disabled Delete button in admin section of this model
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(FcmRegistrationId, FcmRegistrationIdAdmin)
